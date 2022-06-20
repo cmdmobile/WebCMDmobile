32757e94866e4aa9cbffc0285357b92e650261e3
linhchi111750 <linhchi1750@gmail.com>
2022-06-21 03:05:05 +0700

    quantri

--- QUANTRI-CMD/doc/scripheader.js
+++
@@ -1,9 +0,0 @@
-const adressbtn = document.querySelector('#adress-form')
-const adressclose = document.querySelector('#adress-close')
-// thêm sự kiện click cho Hà Nội và nút close
-adressbtn.addEventListener("click",function(){
-    document.querySelector('.adress-form').style.display="flex";
-})
-adressclose.addEventListener("click",function(){
-    document.querySelector('.adress-form').style.display="none";
-})
